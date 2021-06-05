97c3764b550598f083cacf5c339989bd6e40bc41
Mariusz Augustyniak <maugustyniak@gmail.com>
2021-06-05 09:50:22 +0200

    dashboaard styles aside store

---
+++ src/store/layout/mutations.js
@@ -0,0 +1,8 @@
+export default {
+  setAsideLeftVisibility: (state, visibility) => {
+    state.asideLeftVisibility = visibility;
+  },
+  setAsideRightVisibility: (state, visibility) => {
+    state.asideRightVisibility = visibility;
+  },
+};
